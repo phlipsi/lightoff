47ebde48acf2f5790685540fd98f38f64749977f
Philipp Waehnert <philipp.waehnert@posteo.de>
2022-01-30 00:25:31 +0100

    Improves music notation

--- src/level1.asm
+++ src/level1.asm
@@ -21,46 +21,64 @@ level1_custom_wave:
     ;db $ff, $ee, $dd, $cc, $bb, $aa, $99, $88, $77, $66, $55, $44, $33, $22, $11, $00
     db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $00, $00, $00, $00, $00, $00, $00, $00
 
+def NULL   equs "$00, $00, $00, $00"
+def CH2_G6 equs "$36, $f1, $b1, $c6"
+def CH2_A6 equs "$36, $f1, $d6, $c6"
+def CH2_A7 equs "$36, $f1, $6b, $c7"
+def CH2_D6 equs "$36, $f1, $41, $c6"
+def CH2_D7 equs "$36, $f1, $20, $c7"
+def CH2_D8 equs "$36, $f1, $90, $c7"
+
+def CH3_D4 equs "$e0, $20, $41, $c6"
+def CH3_C4 equs "$e0, $20, $0b, $c6"
+def CH3_D5 equs "$e0, $20, $20, $c7"
+def CH3_F4 equs "$e0, $20, $88, $c6"
+
+def CH4_BASE equs  "$2c, $f1, $90, $c0"
+def CH4_HIHAT equs "$3a, $f1, $00, $c0"
+def CH4_SNARE equs "$20, $f1, $50, $80"
+
 level1_music:
-    db $36, $f1, $b1, $c6,    $e0, $20, $41, $c6,    $2c, $f1, $90, $c0
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $2c, $f1, $90, $c0
-    db $36, $f1, $d6, $c6,    $00, $00, $00, $00,    $3a, $f1, $00, $c0
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
+    ;  CH2     CHANNEL 3 (WAVE)       CHANNEL 4 (Perc)
+    db CH2_G6,    CH3_D4,    CH4_BASE
+    db NULL,      NULL,      CH4_BASE
+    db CH2_A6,    NULL,      CH4_HIHAT
+    db NULL,      NULL,      NULL
 
-    db $00, $00, $00, $00,    $e0, $20, $0b, $c6,    $20, $f1, $50, $80
-    db $36, $f1, $b1, $c6,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $36, $f1, $6b, $c7,    $00, $00, $00, $00,    $2c, $f1, $90, $c0
-    db $36, $f1, $41, $c6,    $e0, $20, $41, $c6,    $2c, $f1, $90, $c0
+    db NULL,      CH3_C4,    CH4_SNARE
+    db CH2_G6,    NULL,      NULL
+    db CH2_A7,    NULL,      CH4_BASE
+    db CH2_D6,    CH3_D4,    CH4_BASE
 
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $3a, $f1, $00, $c0
-    db $36, $f1, $6b, $c7,    $00, $00, $00, $00,    $00, $00, $00, $00
+    db NULL,      NULL,      NULL
+    db NULL,      NULL,      NULL
+    db NULL,      NULL,      CH4_HIHAT
+    db CH2_A7,    NULL,      NULL
 
-    db $00, $00, $00, $00,    $e0, $20, $20, $c7,    $20, $f1, $50, $80
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $36, $f1, $6b, $c7,    $00, $00, $00, $00,    $3a, $f1, $00, $c0
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
+    db NULL,      CH3_D5,    CH4_SNARE
+    db NULL,      NULL,      NULL
+    db CH2_A7,    NULL,      CH4_HIHAT
+    db NULL,      NULL,      NULL
 
-    db $00, $00, $00, $00,    $e0, $20, $41, $c6,    $2c, $f1, $90, $c0
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $2c, $f1, $90, $c0
-    db $36, $f1, $6b, $c7,    $e0, $20, $41, $c6,    $3a, $f1, $00, $c0
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
+    db NULL,      CH3_D4,    CH4_BASE
+    db NULL,      NULL,      CH4_BASE
+    db CH2_A7,    CH3_D4,    CH4_HIHAT
+    db NULL,      NULL,      NULL
 
-    db $36, $f1, $20, $c7,    $e0, $20, $88, $c6,    $20, $f1, $50, $80
-    db $36, $f1, $b1, $c6,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $36, $f1, $6b, $c7,    $00, $00, $00, $00,    $2c, $f1, $90, $c0
-    db $00, $00, $00, $00,    $e0, $20, $41, $c6,    $2c, $f1, $90, $c0
+    db CH2_D7,    CH3_F4,    CH4_SNARE
+    db CH2_G6,    NULL,      NULL
+    db CH2_A7,    NULL,      CH4_BASE
+    db NULL,      CH3_D4,    CH4_BASE
 
-    db $36, $f1, $20, $c7,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $00, $00, $00, $00,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $36, $f1, $b1, $c6,    $00, $00, $00, $00,    $3a, $f1, $00, $c0
-    db $36, $f1, $6b, $c7,    $00, $00, $00, $00,    $00, $00, $00, $00
+    db CH2_D7,    NULL,      NULL
+    db NULL,      NULL,      NULL
+    db CH2_G6,    NULL,      CH4_HIHAT
+    db CH2_A7,    NULL,      NULL
 
-    db $00, $00, $00, $00,    $e0, $20, $20, $c7,    $20, $f1, $50, $80
-    db $36, $f1, $20, $c7,    $00, $00, $00, $00,    $00, $00, $00, $00
-    db $36, $f1, $6b, $c7,    $00, $00, $00, $00,    $3a, $f1, $00, $c0
-    db $36, $f1, $90, $c7,    $00, $00, $00, $00,    $00, $00, $00, $00
+    db NULL,      CH3_D5,    CH4_SNARE
+    db CH2_D7,    NULL,      NULL
+    db CH2_A7,    NULL,      CH4_HIHAT
+    db CH2_D8,    NULL,      NULL
 level1_music_end:
 
 load_level1_on_tilemap:
