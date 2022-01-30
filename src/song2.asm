e658069b031ecb94af273cfdea96c4a23288217b
Philipp Waehnert <philipp.waehnert@posteo.de>
2022-01-30 11:36:28 +0100

    Reworks second song

--- src/song2.asm
+++ src/song2.asm
@@ -11,90 +11,108 @@ channel3_wave:
     ;db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $00, $00, $00, $00, $00, $00, $00, $00
 
 song2:
-    ;  SQUARE2    WAVE       PERC
-    ; 1 -------------------------
-    db CH2, G2,   NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2, D3,   CH3, G6,   SH
-    db CH2, D3,   NULL,      NULL
-
-    db NULL,      CH3, D6,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, D3,   CH3, C6,   SH
-    db NULL,      NULL,      NULL
-
-    db CH2, AIS2, CH3, D6,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, G3,   CH3, AIS5, SH
-    db CH2, G3,   NULL,      NULL
-
-    db NULL,      CH3, A5,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, AIS2, CH3, G5,   SH
-    db NULL,      NULL,      NULL
-
-    db CH2, A2,   CHL3,FIS5, NULL
-    db NULL,      NULL,      NULL
-    db CH2, D3,   NULL,      SH
-    db CH2, D3,   CH3, D6,   NULL
-
-    db NULL,      CHL3, D6,  NULL
-    db NULL,      NULL,      NULL
-    db CH2, A2,   NULL,      SH
-    db NULL,      CH3, D6,   NULL
-
-    db CH2, D2,   NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2, A2,   NULL,      SH
-    db CH2, A2,   NULL,      NULL
-
-    db NULL,      NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2, AIS2, NULL,      SH
-    db NULL,      NULL,      NULL
-    ; ---------------------------
-    db CH2, A2,   NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2, D3,   CH3, FIS6, SH
-    db CH2, D3,   NULL,      NULL
-
-    db NULL,      CH3, D6,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, A2,   CH3, C6,   SH
-    db NULL,      NULL,      NULL
-
-    db CH2, D2,   CH3, D6,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, A2,   CH3, AIS5, SH
-    db CH2, A2,   NULL,      NULL
-
-    db NULL,      CH3, A5,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, AIS2, CH3, FIS5, SH
-    db NULL,      NULL,      NULL
-
-    db CH2, AIS2, CHL3,G5,   NULL
-    db NULL,      NULL,      NULL
-    db CH2, G3,   NULL,      SH
-    db CH2, G3,   CH3, D6,   NULL
-
-    db NULL,      CHL3, D6,  NULL
-    db NULL,      NULL,      NULL
-    db CH2, AIS2, NULL,      SH
-    db NULL,      CH3, D6,   NULL
-
-    db CH2, G2,   NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2, D3,   NULL,      SH
-    db CH2, D3,   NULL,      NULL
-
-    db NULL,      NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2, D3,   NULL,      SH
-    db NULL,      NULL,      NULL
-
-    ; ===========================
+    dw intro_base,  intro_lead,  intro_perc
+    dw intro_base2, intro_lead2, intro_perc
+    dw intro_base,  intro_lead3,  intro_perc
+    dw intro_base3, intro_lead4, intro_perc2
 song2_end:
+
+intro_base:
+    db CH2, G2,   NULL,      CH2, D3,   CH2, D3
+    db NULL,      NULL,      CH2, D3,   NULL
+    db CH2, AIS2, NULL,      CH2, G3,   CH2, G3
+    db NULL,      NULL,      CH2, AIS2, NULL
+
+    db CH2, A2,   NULL,      CH2, D3,   CH2, D3
+    db NULL,      NULL,      CH2, A2,   NULL
+    db CH2, D2,   NULL,      CH2, A2,   CH2, A2
+    db NULL,      NULL,      CH2, AIS2, NULL
+
+intro_base2:
+    db CH2, A2,   NULL,      CH2, D3,   CH2, D3
+    db NULL,      NULL,      CH2, A2,   NULL
+    db CH2, D2,   NULL,      CH2, A2,   CH2, A2
+    db NULL,      NULL,      CH2, AIS2, NULL
+
+    db CH2, AIS2, NULL,      CH2, G3,   CH2, G3
+    db NULL,      NULL,      CH2, AIS2, NULL
+    db CH2, G2,   NULL,      CH2, D3,   CH2, D3
+    db NULL,      NULL,      CH2, D3,   NULL
+
+intro_base3:
+    db CH2, FIS2, NULL,      CH2, D3,   CH2, D3
+    db NULL,      NULL,      CH2, FIS2, NULL
+    db CH2, G2,   NULL,      CH2, D2,   CH2, D2
+    db NULL,      NULL,      CH2, D2,   NULL
+
+    db CH2, D2,   NULL,      CH2, FIS2, CH2, FIS2
+    db NULL,      NULL,      CH2, D2,   NULL
+    db CH2, G2,   NULL,      CH2, D3,   CH2, D3
+    db NULL,      NULL,      CH2, D3,   NULL
+
+intro_lead:
+    db NULL,      NULL,      CH3, G6,   NULL
+    db CH3, D6,   NULL,      CH3, C6,   NULL
+    db CH3, D6,   NULL,      CH3, AIS5, NULL
+    db CH3, A5,   NULL,      CH3, G5,   NULL
+
+    db CHL3,FIS5, NULL,      NULL,      CH3, D6
+    db CHL3, D6,  NULL,      NULL,      CH3, D6
+    db NULL,      NULL,      NULL,      NULL
+    db NULL,      NULL,      NULL,      NULL
+
+intro_lead2:
+    db NULL,      NULL,      CH3, FIS6, NULL
+    db CH3, D6,   NULL,      CH3, C6,   NULL
+    db CH3, D6,   NULL,      CH3, AIS5, NULL
+    db CH3, A5,   NULL,      CH3, FIS5, NULL
+
+    db CHL3, G5,  NULL,      NULL,      CH3, D6
+    db CHL3, D6,  NULL,      NULL,      CH3, D6
+    db NULL,      NULL,      NULL,      NULL
+    db NULL,      NULL,      NULL,      NULL
+
+intro_lead3:
+    db NULL,      NULL,      CH3, G6,   NULL
+    db CH3, D6,   NULL,      CH3, C6,   NULL
+    db CH3, D6,   NULL,      CH3, AIS5, NULL
+    db CH3, A5,   NULL,      CH3, G5,   NULL
+
+    db CHL3,DIS6, NULL,      NULL,      CH3, C6
+    db CHL3, C6,  NULL,      NULL,      CH3, DIS6
+    db NULL,      NULL,      NULL,      NULL
+    db NULL,      NULL,      NULL,      NULL
+
+intro_lead4:
+    db NULL,      NULL,      CH3, DIS6, NULL
+    db CH3, D6,   NULL,      CH3, C6,   NULL
+    db CH3, D6,   NULL,      CH3, AIS5, NULL
+    db CH3, G5,   NULL,      CH3, AIS5, NULL
+
+    db CHL3, A5,  NULL,      NULL,      NULL
+    db CHL3, FIS5,NULL,      NULL,      NULL
+    db CHL3, G5,  NULL,      NULL,      NULL
+    db NULL,      NULL,      NULL,      NULL
+
+intro_perc:
+    db BD, HH,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+    db BD, HH,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+    db BD, HH,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+    db BD, HH,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+
+intro_perc2:
+    db BD, HH,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+    db BD, HH,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+    db SN, NULL,   SH,   NULL
+    db NULL, NULL,   SH,   NULL
+    db NULL, NULL, SH, NULL
+    db NULL, NULL, SN, SN
 
 section "SONG2_LOADER", rom0
 
@@ -106,7 +124,7 @@ load_song2::
     ld de, channel3_wave
     call load_custom_wave
     ld bc, song2
-    ld de, (song2_end - song2) / 12
-
+    ld d, (song2_end - song2) / (2 * 3)
+    ld e, 32
     ld hl, $e018
     jp init_song
