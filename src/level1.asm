a5c3676ca08fbfffc32e15c4b8ed38a1566a05aa
Philipp Waehnert <philipp.waehnert@posteo.de>
2022-01-30 01:07:39 +0100

    Restructures first song

--- src/level1.asm
+++ src/level1.asm
@@ -17,70 +17,6 @@ level1_off:
     incbin "level1-off.tilemap", 0, 32 * 32
 level1_off_end:
 
-level1_custom_wave:
-    ;db $ff, $ee, $dd, $cc, $bb, $aa, $99, $88, $77, $66, $55, $44, $33, $22, $11, $00
-    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $00, $00, $00, $00, $00, $00, $00, $00
-
-def NULL   equs "$00, $00, $00, $00"
-def CH2_G6 equs "$36, $f1, $b1, $c6"
-def CH2_A6 equs "$36, $f1, $d6, $c6"
-def CH2_A7 equs "$36, $f1, $6b, $c7"
-def CH2_D6 equs "$36, $f1, $41, $c6"
-def CH2_D7 equs "$36, $f1, $20, $c7"
-def CH2_D8 equs "$36, $f1, $90, $c7"
-
-def CH3_D4 equs "$e0, $20, $41, $c6"
-def CH3_C4 equs "$e0, $20, $0b, $c6"
-def CH3_D5 equs "$e0, $20, $20, $c7"
-def CH3_F4 equs "$e0, $20, $88, $c6"
-
-def CH4_BASE equs  "$2c, $f1, $90, $c0"
-def CH4_HIHAT equs "$3a, $f1, $00, $c0"
-def CH4_SNARE equs "$20, $f1, $50, $80"
-
-level1_music:
-    ;  CH2     CHANNEL 3 (WAVE)       CHANNEL 4 (Perc)
-    db CH2_G6,    CH3_D4,    CH4_BASE
-    db NULL,      NULL,      CH4_BASE
-    db CH2_A6,    NULL,      CH4_HIHAT
-    db NULL,      NULL,      NULL
-
-    db NULL,      CH3_C4,    CH4_SNARE
-    db CH2_G6,    NULL,      NULL
-    db CH2_A7,    NULL,      CH4_BASE
-    db CH2_D6,    CH3_D4,    CH4_BASE
-
-    db NULL,      NULL,      NULL
-    db NULL,      NULL,      NULL
-    db NULL,      NULL,      CH4_HIHAT
-    db CH2_A7,    NULL,      NULL
-
-    db NULL,      CH3_D5,    CH4_SNARE
-    db NULL,      NULL,      NULL
-    db CH2_A7,    NULL,      CH4_HIHAT
-    db NULL,      NULL,      NULL
-
-    db NULL,      CH3_D4,    CH4_BASE
-    db NULL,      NULL,      CH4_BASE
-    db CH2_A7,    CH3_D4,    CH4_HIHAT
-    db NULL,      NULL,      NULL
-
-    db CH2_D7,    CH3_F4,    CH4_SNARE
-    db CH2_G6,    NULL,      NULL
-    db CH2_A7,    NULL,      CH4_BASE
-    db NULL,      CH3_D4,    CH4_BASE
-
-    db CH2_D7,    NULL,      NULL
-    db NULL,      NULL,      NULL
-    db CH2_G6,    NULL,      CH4_HIHAT
-    db CH2_A7,    NULL,      NULL
-
-    db NULL,      CH3_D5,    CH4_SNARE
-    db CH2_D7,    NULL,      NULL
-    db CH2_A7,    NULL,      CH4_HIHAT
-    db CH2_D8,    NULL,      NULL
-level1_music_end:
-
 load_level1_on_tilemap:
     ld de, level1_on
     ld hl, _SCRN0
@@ -94,11 +30,7 @@ load_level1_off_tilemap:
     jp copy_memory
 
 level1::
-    ld de, level1_custom_wave
-    call load_custom_wave
-    ld bc, level1_music
-    ld d, (level1_music_end - level1_music) / 12
-    call init_song
+    call load_song1
 
     ld hl, MAX_CAMERA_X
     ld [hl], SCRN_VX - SCRN_X
---
+++ src/song1.asm
@@ -0,0 +1,60 @@
+include "notes.inc"
+
+section "SONG1", rom0
+
+def CH2  equs "$31, $f1"
+def CH3  equs "$e0, $20"
+
+channel3_wave:
+    ;db $ff, $ee, $dd, $cc, $bb, $aa, $99, $88, $77, $66, $55, $44, $33, $22, $11, $00
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $00, $00, $00, $00, $00, $00, $00, $00
+
+song1:
+    ;  SQUARE2    WAVE       PERC
+    db CH2, G4,   CH3, D4,   SH
+    db NULL,      NULL,      SH
+    db CH2, A4,   NULL,      HH
+    db NULL,      NULL,      NULL
+
+    db NULL,      CH3, C4,   SN
+    db CH2, G4,   NULL,      NULL
+    db CH2, A5,   NULL,      SH
+    db CH2, D4,   CH3, D4,   SH
+
+    db NULL,      NULL,      NULL
+    db NULL,      NULL,      NULL
+    db NULL,      NULL,      HH
+    db CH2, A5,   NULL,      NULL
+
+    db NULL,      CH3, D5,   SN
+    db NULL,      NULL,      NULL
+    db CH2, A5,   NULL,      HH
+    db NULL,      NULL,      NULL
+
+    db NULL,      CH3, D4,   SH
+    db NULL,      NULL,      SH
+    db CH2, A5,   CH3, D4,   HH
+    db NULL,      NULL,      NULL
+
+    db CH2, D5,   CH3, F4,   SN
+    db CH2, D4,   NULL,      NULL
+    db CH2, A5,   NULL,      SH
+    db NULL,      CH3, D4,   SH
+
+    db CH2, D5,   NULL,      NULL
+    db NULL,      NULL,      NULL
+    db CH2, D4,   NULL,      HH
+    db CH2, A5,   NULL,      NULL
+
+    db NULL,      CH3, D5,   SN
+    db CH2, D5,   NULL,      NULL
+    db CH2, A5,   NULL,      HH
+    db CH2, D6,   NULL,      NULL
+song1_end:
+
+load_song1::
+    ld de, channel3_wave
+    call load_custom_wave
+    ld bc, song1
+    ld d, (song1_end - song1) / 12
+    jp init_song
